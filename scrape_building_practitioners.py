eb798eaee89e0d8be3d85e1a389107cc5018a814
saranoor <saranoor@live.com>
2026-01-14 14:06:50 +0500

    remove redundant code

--- scrape_building_practitioners.py
+++ scrape_building_practitioners.py
@@ -109,9 +109,7 @@ while (i<10):
         print("Phone not found")
         phone = "N/A"
 
-    # get_practitioner_details(link)
     driver.get(link)
-    # Initialize Shadow helper
     shadow = Shadow(driver)
 
     time.sleep(3)  # Wait for the page to load completely
@@ -128,91 +126,17 @@ while (i<10):
             parent = shadow.get_parent_element(label) 
             value_item = shadow.get_next_sibling_element(parent)
             address = value_item.text.strip().replace('\n', ', ')
-            
-        elif text == "Contact Details":
-            # Same logic for Contact Details
-            parent = shadow.get_parent_element(label)
-            value_item = shadow.get_next_sibling_element(parent)
-            phone = value_item.text.strip()
 
     print(f"Address: {address}")
     print(f"Phone: {phone}")
-    # numbers = driver.execute_script("""
-    # function collect(root, results) {
-    #     if (!root) return;
-
-    #     root.querySelectorAll('li.search-result-text-style')
-    #         .forEach(li => {
-    #             const text = li.textContent.trim();
-    #             if (text) results.push(text);
-    #         });
-
-    #     for (const el of root.querySelectorAll('*')) {
-    #         if (el.shadowRoot) {
-    #             collect(el.shadowRoot, results);
-    #         }
-    #     }
-    # }
-
-    # const results = [];
-    # collect(document, results);
-    # return results;
-    # """)
-
-    # print(numbers)
-    # def extract_phone(items):
-    #     for item in items:
-    #         # Case 1: "Phone Number 088497890"
-    #         if item.startswith("Phone Number"):
-    #             parts = item.replace("Phone Number", "").strip()
-    #             if parts:  # number exists
-    #                 return parts
-    #             else:
-    #                 return ''
-    #     return None
-    # phone = extract_phone(numbers)
-
-    # print(f"phone", phone)
-    # df.loc[i,"phone"] = phone
-
-    # link = driver.execute_script("""
-    # function findResultLink(root) {
-    #     if (!root) return null;
-
-    #     const a = root.querySelector('a.search-result-name-text-style');
-    #     if (a) return a;
-
-    #     for (const el of root.querySelectorAll('*')) {
-    #         if (el.shadowRoot) {
-    #             const found = findResultLink(el.shadowRoot);
-    #             if (found) return found;
-    #         }
-    #     }
-    #     return null;
-    # }
-    # return findResultLink(document);
-    # """)
-
-    # if not link:
-    #     print("No practitioner found")
-    # else:
-    #     # driver.execute_script("arguments[0].click();", link)
-    #     print("Opening link:", link.get_attribute("href"))
-    #     address, contact_details = get_practitioner_details(link.get_attribute("href"))
-    #     # go open the link in new tab
-
-    # name = link.text.strip()
-    # href = link.get_attribute("href")
-    # df.loc[i,"namae"] = name
-    # df.loc[i,"contact details"] = contact_details
-    # df.loc[i,"business details"] = address
+    df.loc[i,"namae"] = name
+    df.loc[i,"contact details"] = phone
+    df.loc[i,"business details"] = address
 
     i+=1
     driver.back()
     print("Going back to search page")
     time.sleep(20)
-
-
 
 print(df.head(10))
 time.sleep(20)
